4991c96233c05f80d5fb10f7c798d2379d4b2a61
williamcruzme <john.cruz@elaniin.com>
2019-10-01 22:16:03 -0600

    🎨 Sidebar items active style improved

--- src/components/sidebar/Item.vue
+++ src/components/sidebar/Item.vue
@@ -5,6 +5,7 @@
     :to="to"
     class="flex items-center w-full h-12 pl-6 cursor-pointer border-l-2 hover:bg-gray-100 hover:border-purple"
     active-class="bg-gray-100 border-purple"
+    exact
   >
     <figure class="flex justify-center mr-4 w-6">
       <img
